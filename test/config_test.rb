4aec6fcbe7488b08d59012dc36fe3c12e0a84886
Michael Fitz-Payne <fitzy@discourse.org>
2021-05-20 13:08:28 +1000

    Update to different multiline syntax to fix weird indenting

--- test/config_test.rb
+++ test/config_test.rb
@@ -12,12 +12,12 @@ module Pups
 
     def test_env_param
       ENV["FOO"] = "BAR"
-      config = <<YAML
-env:
-  BAR: baz
-  hello: WORLD
-  one: 1
-YAML
+      config = <<~YAML
+      env:
+        BAR: baz
+        hello: WORLD
+        one: 1
+      YAML
 
       config = Config.new(YAML.load(config))
       assert_equal("BAR", config.params["$ENV_FOO"])
@@ -28,15 +28,15 @@ YAML
 
     def test_env_with_template
       ENV["FOO"] = "BAR"
-      config = <<YAML
-env:
-  greeting: "{{hello}}, {{planet}}!"
-  one: 1
-  other: "where are we on {{planet}}?"
-env_template:
-  planet: pluto
-  hello: hola
-YAML
+      config = <<~YAML
+      env:
+        greeting: "{{hello}}, {{planet}}!"
+        one: 1
+        other: "where are we on {{planet}}?"
+      env_template:
+        planet: pluto
+        hello: hola
+      YAML
       config_hash = YAML.load(config)
 
       config = Config.new(config_hash)
@@ -48,15 +48,15 @@ YAML
 
     def test_env_with_ENV_templated_variable
       ENV["env_template_config"] = "my_application"
-      config = <<YAML
-env:
-  greeting: "{{hello}}, {{planet}}!"
-  one: 1
-  other: "building {{config}}"
-env_template:
-  planet: pluto
-  hello: hola
-YAML
+      config = <<~YAML
+      env:
+        greeting: "{{hello}}, {{planet}}!"
+        one: 1
+        other: "building {{config}}"
+      env_template:
+        planet: pluto
+        hello: hola
+      YAML
       config_hash = YAML.load(config)
 
       config = Config.new(config_hash)
@@ -70,15 +70,15 @@ YAML
       f = Tempfile.new("test")
       f.close
 
-      config = <<YAML
-env:
-  PLANET: world
-params:
-  run: #{f.path}
-  greeting: hello
-run:
-  - exec: echo $greeting $PLANET >> #{f.path}
-YAML
+      config = <<~YAML
+      env:
+        PLANET: world
+      params:
+        run: #{f.path}
+        greeting: hello
+      run:
+        - exec: echo $greeting $PLANET >> #{f.path}
+      YAML
 
       Config.new(YAML.load(config)).run
       assert_equal("hello world", File.read(f.path).strip)
@@ -88,26 +88,23 @@ YAML
     end
 
     def test_hooks
-      yaml = <<YAML
-run:
-  - exec: 1
-  - exec:
-      hook: middle
-      cmd: 2
-  - exec: 3
-hooks:
-  after_middle:
-    - exec: 2.1
-  before_middle:
-    - exec: 1.9
-YAML
+      yaml = <<~YAML
+      run:
+        - exec: 1
+        - exec:
+            hook: middle
+            cmd: 2
+        - exec: 3
+      hooks:
+        after_middle:
+          - exec: 2.1
+        before_middle:
+          - exec: 1.9
+      YAML
 
       config = Config.load_config(yaml).config
       assert_equal({ "exec" => 1.9 }, config["run"][1])
       assert_equal({ "exec" => 2.1 }, config["run"][3])
-
-
     end
   end
 end
-
